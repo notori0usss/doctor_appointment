10fdd18b598d506c5fe720c30af600c979824b25
Swaroop Dhungana <dhunganaswaroop@gmail.com>
2022-12-11 18:25:47 +0545

    Init

---
+++ lib/utils/text.dart
@@ -0,0 +1,11 @@
+class AppText {
+  static final enText = {
+    'welcome_text': 'Welcome',
+    'signIn_text': 'Sign in to your account',
+    'registered_text': 'Already registered',
+    'register_text': 'You can easily sign up, and connect to Doctors ',
+    'signUp_text': 'Don\'t have an account?',
+    'social_login': 'Or continue with your social account',
+    'forgot_password': 'Forgot your password',
+  };
+}
